9b9f3175c79d50c929ab46a48c1d4a44c4b1f98b
Sergey Yarlykov <siyarlykov@mts.ru>
2021-10-18 21:04:31 +0300

    up to v1.1.5 (pagination)

--- SmartAdapter/src/main/java/s/yarlykov/lib/smartadapter/extensions/FlowExt.kt
+++ SmartAdapter/src/main/java/s/yarlykov/lib/smartadapter/extensions/FlowExt.kt
@@ -1,4 +1,27 @@
 package s.yarlykov.lib.smartadapter.extensions
 
-class FlowExt {
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.plus
+
+val defaultExceptionHandler = CoroutineExceptionHandler { _, _ -> }
+
+fun appMainScope(): CoroutineScope = MainScope() + defaultExceptionHandler
+
+fun <T> Flow<T>.throttleFirst(periodMillis: Long): Flow<T> {
+
+    return flow {
+        var lastTime = 0L
+        this@throttleFirst.collect { value ->
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastTime >= periodMillis) {
+                lastTime = currentTime
+                emit(value)
+            }
+        }
+    }
 }
--- SmartAdapter/src/main/java/s/yarlykov/lib/smartadapter/pagination/PagedSmartAdapter.kt
+++ SmartAdapter/src/main/java/s/yarlykov/lib/smartadapter/pagination/PagedSmartAdapter.kt
@@ -3,89 +3,86 @@ package s.yarlykov.lib.smartadapter.pagination
 import androidx.core.view.doOnNextLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import s.yarlykov.lib.smartadapter.adapter.SmartAdapter
 import s.yarlykov.lib.smartadapter.events.EventWrapper
+import s.yarlykov.lib.smartadapter.extensions.appMainScope
+import s.yarlykov.lib.smartadapter.extensions.throttleFirst
 
-//  todo: Кусок кода пагинации из проекта IziMaster::ChatActivity.kt
-//
-//  private val swipeRefreshListener: () -> Unit = {
-//      if (!isUpdating) {
-//          isUpdating = true
-//
-//          todo Если текущее количество показанных сообщений кратно CHAT_PAGE_SIZE,
-//          todo то существует вероятность, что ещё не все сообщения чата скачаны.
-//          if (model.isNotEmpty() && (topicId != 0) && (model.size % CHAT_PAGE_SIZE == 0)) {
-//              viewModel.requestMorePages(topicId, model.size / CHAT_PAGE_SIZE + 1)
-//          } else {
-//              isUpdating = false
-//          }
-//      }
-//  }
-
-/**
- * Используется RecyclerView.OnScrollListener, который следит за приближением к хвосту списка
- * при прокрутке. Если осталось непросмотрено N элементов, то выдается сигнал TakeMore. Сообщение
- * TakeMore уходит во внешний мир в обертке EventWrapper<TakeMore> через коллекторы. Внешний
- * код реагирует и подкачивает следующую порцию данных.
- *
- * Вариант реализации TakeMore, где lastIndex - длина текущей модели:
- *      data class TakeMore(val lastIndex: Int)
- */
 
 /**
  * TODO 1:
- * Возможность начальной загрузки с произвольного индекса, а не только в 0. Это может пригодиться,
- * если приложение было убито и восстановлено системой. Тогда придется сделать пагинацию "вверх",
- * т.е. в сторону индекса 0. Также придется хранить в onSaveInstanceState последнее значение
- * "верхнего" индекса. ViewModel как хранилка не поможет.
+ * Возможность начальной загрузки с произвольного индекса, а не только с 0 (может пригодиться,
+ * если приложение восстановлено и нужно вернуть UI в прежнее состояние). Также придется хранить
+ * в onSaveInstanceState последнее значение "верхнего" индекса. ViewModel как хранилка не поможет.
+ *
+ * TODO 2:
+ * Добавить поддержку GridLayoutManager в bindPaginationListener. Там придется высчитывать с
+ * учетом количества элементов в строке.
  *
  * @property prefetchDistance - размер страницы
  */
-class PagedSmartAdapter(private val prefetchDistance: Int) : SmartAdapter() {
+@ExperimentalCoroutinesApi
+class PagedSmartAdapter(private val prefetchDistance: Int) :
+    SmartAdapter(), CoroutineScope by appMainScope() {
+
+    private var job: Job? = null
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         super.onAttachedToRecyclerView(recyclerView)
-
-        /**
-         * NOTE: Если адаптер назначен раньше, чем LayoutManager, то мы получим Exception
-         * в initPaginationListener, потому что там val layoutManager окажется равной null.
-         * Поэтому подписываемся на следующий layout RecyclerView и уже после выполняем
-         * инициализацию пагинации.
-         *
-         * Эта подписка автоматически удаляется после первого срабатывания.
-         */
         recyclerView.doOnNextLayout {
-            bindPaginationListener(recyclerView)
+            job = bindPaginationListener(recyclerView)
         }
     }
 
-    /**
-     * TODO Пока работаем только с LinearLayoutManager.
-     *
-     * Также надо добавить поддержку GridLayoutManager. Там придется высчитывать с учетом
-     * количества элементов в строке.
-     */
-    private fun bindPaginationListener(recyclerView: RecyclerView) {
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        job?.cancel()
+    }
+
+    private fun bindPaginationListener(recyclerView: RecyclerView): Job {
         val layoutManager = (recyclerView.layoutManager as? LinearLayoutManager)
-            ?: throw Exception("Only LinearLayoutManager is supported")
+            ?: throw Exception(EX_LM_RESTRICTIONS)
 
-        /**
-         * Если OnScrollListener сработал, значит что-то прокрутили, значит в адаптере
-         * есть какая-то модель. Поэтому itemCount должен быть больше 0. Но на всякий случай...
-         */
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
+        return launch {
+            pagingFlow(recyclerView, layoutManager).throttleFirst(THROTTLE_TIMEOUT)
+                .collect { event -> collector.collectorFlow.tryEmit(event) }
+        }
+    }
 
-                val lastModePosition = if (itemCount > 0) itemCount - 1 else 0
-                val lastVisiblePosition = layoutManager.findLastVisibleItemPosition()
+    private fun pagingFlow(recyclerView: RecyclerView, layoutManager: LinearLayoutManager) =
+        callbackFlow {
 
-                if (lastModePosition - lastVisiblePosition < prefetchDistance) {
-                    val event = EventWrapper(TakeMore(lastVisiblePosition))
-                    collector.collectorFlow.tryEmit(event)
-                    collector.collectorRx.onNext(event)
+            val scrollListener = object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    // Прокрутку к верхним элементам игнорировать
+                    if (dy <= 0) {
+                        return
+                    }
+
+                    val lastModelPosition = if (itemCount > 0) itemCount - 1 else 0
+                    val lastVisiblePosition = layoutManager.findLastVisibleItemPosition()
+
+                    if (lastModelPosition - lastVisiblePosition < prefetchDistance) {
+                        trySend(EventWrapper(TakeMore(lastVisiblePosition)))
+                    }
                 }
             }
-        })
+
+            recyclerView.addOnScrollListener(scrollListener)
+            awaitClose { recyclerView.removeOnScrollListener(scrollListener) }
+        }
+
+    companion object {
+        private const val EX_LM_RESTRICTIONS = "Only LinearLayoutManager is supported"
+        private const val THROTTLE_TIMEOUT = 1500L
     }
 }
